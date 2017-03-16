5a972517a0642c9a7a9b9029b8f4e30fb8c831de
tanmoy12 <tanmoy.buet12@gmail.com>
2017-03-16 20:43:27 +0600

    signup changed in files

--- imports/ui/components/SignUp.jsx
+++ imports/ui/components/SignUp.jsx
@@ -1,23 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export default class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            message: '',
-            type: 'init',
-            bgColorComp: '#337ab7',
-            bgColorEmpl: '#337ab7'
-
+            message: ''
         };
     }
-
-
-    handleRegister(e) {
+    handleRegister(e){
         e.preventDefault();
 
-        that = this;
+        that=this;
         var username = ReactDOM.findDOMNode(this.refs.username).value.trim();
         var email = ReactDOM.findDOMNode(this.refs.email).value.trim();
         var password = ReactDOM.findDOMNode(this.refs.password).value.trim();
@@ -25,7 +19,7 @@ export default class SignUp extends Component {
 
         console.log(designation);
 
-        var User = {
+        var User ={
             username: username,
             email: email,
             password: password,
@@ -34,280 +28,50 @@ export default class SignUp extends Component {
             }
         };
         Accounts.createUser(User, function (err) {
-            if (err) {
+            if(err){
                 that.setState({
                     message: err.reason
                 });
             }
         });
 
-    }
-
-
-    compClicked() {
-        this.setState({
-            type: 'comp',
-            bgColorComp: 'darkred',
-            bgColorEmpl: '#337ab7'
-        });
     };
-
-    emplClicked() {
-        this.setState({
-            type: 'empl',
-            bgColorEmpl: 'darkred',
-            bgColorComp: '#337ab7'
-        });
-    };
-
 
     render() {
-        var fullWidth = {
-            width: "100%"
-        };
+        return (
+            <div className="container">
+                <form onSubmit={this.handleRegister.bind(this)}>
+                    <div className="col-sm-9">
+                        <div className="form-group">
 
-        var topMarign = {
-            marginTop: "8%",
-            marginLeft: "42%"
-        };
+                            <input ref="username" type="text" placeholder="Username"
+                                   className="form-control"/>
 
-        var inputFileWidth = {
-            width: "33%"
-        }
-
-
-        if (this.state.type == 'comp') {
-            //console.log("HIIII");
-            return (
-                <div className="container">
-                    <div className="login">
-                        <h2 className="signUpHeader">Sign Up</h2>
-                        <div className="btn-group btn-group-justified" role="group" aria-label="...">
-                            <div className="btn-group" role="group">
-                                <button type="button" className="btn btn-primary"
-                                        onClick={this.compClicked.bind(this)}
-                                        style={{backgroundColor: this.state.bgColorComp}}>Company
-                                </button>
-                            </div>
-                            <div className="btn-group" role="group">
-                                <button type="button" className="btn btn-primary"
-                                        onClick={this.emplClicked.bind(this)}
-                                        style={{backgroundColor: this.state.bgColorEmpl}}>Employee
-                                </button>
-                            </div>
                         </div>
 
-                        <div >
 
-
-
-                            <div className="row">
-                                <div className="col-md-7">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="signUpInput" ref='companyname' placeholder='Company Name' type='text'/>
-
-
-                                    </div>
-                                </div>
-                                <div className="col-md-5">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="fileInput signUpInput"  id="f02" type="file" name="companylogo"
-                                               accept="application/pdf"/>
-                                        <label className="fileInputLabel signUpInput" for="f02">Add Company Logo</label>
-                                    </div>
-                                </div>
-                            </div>
-
-
-
-
-
-
-                            <div className="row">
-                                <div className="col-md-7">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="signUpInput" ref='tradelicenseno'
-                                               placeholder='Trade License No.'
-                                               type='text'/>
-
-
-                                    </div>
-                                </div>
-                                <div className="col-md-5">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="fileInput signUpInput"  id="f01" type="file" name="tradelicensepdf"
-                                               accept="application/pdf"/>
-                                        <label className="fileInputLabel signUpInput" for="f01">Add Trade License</label>
-                                    </div>
-                                </div>
-                            </div>
-
-
-
-                            <div className="row">
-                                <div className="col-md-7">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="signUpInput" ref='ownername'
-                                               placeholder='Owner Name' type='text'/>
-
-
-                                    </div>
-                                </div>
-                                <div className="col-md-5">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="fileInput signUpInput"  id="f03" type="file" name="solvencypaperpdf"
-                                               accept="application/pdf"/>
-                                        <label className="fileInputLabel signUpInput" for="f03">Add Solvency Paper</label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <textarea className="signUpInput" id="description" rows="5" cols="70"
-                                      placeholder="Description"></textarea>
-
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="signUpInput" ref='email' placeholder='Email' type='email'/>
-
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="signUpInput" ref='mobno' placeholder='Mobile No' type='text'/>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="signUpInput" ref='username' placeholder='Username'
-                                               type='text'/>
-
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="signUpInput" ref='password' placeholder='Password'
-                                               type='password'/></div>
-                                </div>
-                            </div>
-
-
-                            {/*<div className="row">*/}
-                                {/*<div className="col-md-6">*/}
-                                    {/*<div className="input-group input-group-lg" style={fullWidth}>*/}
-                                        {/*<input className="signUpInput" type="file" name="signature" accept="image/*"/>*/}
-
-                                    {/*</div>*/}
-                                {/*</div>*/}
-                                {/*<div className="col-md-6">*/}
-                                    {/*<div className="input-group input-group-lg" style={fullWidth}>*/}
-                                        {/*<input className="signUpInput" type="file" name="seal" accept="image/*"/></div>*/}
-                                {/*</div>*/}
-                            {/*</div>*/}
-
-
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="fileInput signUpInput" id="f04" type="file" name="signature" accept="image/*"/>
-                                        <label className="fileInputLabel signUpInput" for="f04">Add Signature</label>
-
-
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="input-group input-group-lg" style={fullWidth}>
-                                        <input className="fileInput signUpInput" type="file" id="f05" name="seal" accept="image/*"/>
-
-                                        <label className="fileInputLabel signUpInput" for="f05">Add Seal</label>
-                                    </div>
-                                </div>
-                            </div>
-
-
-
-
-                            <button type="submit" className="btn btn-md btn-success signUpSubmit" style={topMarign}>
-                                Sign Up
-                            </button>
-                            <br/>
-                            <p>{this.state.message}</p>
+                        <div className="form-group">
+                            <input type="text" placeholder="Email or mobile number" ref="email"
+                                   className="form-control"/>
                         </div>
+                        <div className="form-group">
+                            <input type="password" placeholder="New password" ref="password"
+                                   className="form-control"/>
+                        </div>
+                        <div className="form-group col-md-10 col-sm-6">
+                            <select ref="designation" className="form-control">
+                                <option>Scientific Officer</option>
+                                <option>Accounting Officer</option>
+                                <option>Director</option>
+                            </select>
+                        </div>
+                        <button type="submit" className="btn btn-md btn-success">Sign Up</button>
+                        <br/>
+                        <p>{this.state.message}</p>
                     </div>
-                </div>
-            );
-        }
-        if (this.state.type == 'init') {
-            return (
-                <div className="container">
-                    <form onSubmit={this.handleRegister.bind(this)}>
-                        <div className="login">
+                </form>
 
-                            <h2 className="signUpHeader">Sign Up</h2>
-
-                            <div className="btn-group btn-group-justified" role="group" aria-label="...">
-                                <div className="btn-group" role="group">
-                                    <button type="button" className="btn btn-primary"
-                                            onClick={this.compClicked.bind(this)}
-                                            style={{backgroundColor: this.state.bgColorComp}}>Company
-                                    </button>
-                                </div>
-                                <div className="btn-group" role="group">
-                                    <button type="button" className="btn btn-primary"
-                                            onClick={this.emplClicked.bind(this)}
-                                            style={{backgroundColor: this.state.bgColorEmpl}}>Employee
-                                    </button>
-                                </div>
-                            </div>
-
-
-                        </div>
-                    </form>
-
-                </div>
-            );
-        }
-
-        if (this.state.type == 'empl') {
-            return (
-                <div className="container">
-                    <form onSubmit={this.handleRegister.bind(this)}>
-                        <div className="login">
-
-                            <h2 className="signUpHeader">Sign Up</h2>
-
-                            <div className="btn-group btn-group-justified" role="group" aria-label="...">
-                                <div className="btn-group" role="group">
-                                    <button type="button" className="btn btn-primary"
-                                            onClick={this.compClicked.bind(this)}
-                                            style={{backgroundColor: this.state.bgColorComp}}>Company
-                                    </button>
-                                </div>
-                                <div className="btn-group" role="group">
-                                    <button type="button" className="btn btn-primary"
-                                            onClick={this.emplClicked.bind(this)}
-                                            style={{backgroundColor: this.state.bgColorEmpl}}>Employee
-                                    </button>
-                                </div>
-                            </div>
-
-                            <div className="signUpOption">
-                                <select ref="designation" className="optionStyle">
-                                    <option className="optionStyle">Scientific Officer</option>
-                                    <option className="optionStyle">Accounting Officer</option>
-                                    <option className="optionStyle">Director</option>
-                                </select>
-                            </div>
-
-                        </div>
-                    </form>
-
-                </div>
-            );
-        }
+            </div>
+        );
     }
 }
