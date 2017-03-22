cd2883bcd55d35705a24874c9733fd5010468f9a
BingoApps <l.hadima23@gmail.com>
2017-03-22 06:34:08 +0600

    RFQ box redesigned

--- imports/ui/components/RFQBox.jsx
+++ imports/ui/components/RFQBox.jsx
@@ -12,73 +12,41 @@ export default class RFQBox extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div id="sidejumbotron" className="col-md-6 jumbotron">
-                            <nav className=" navbar-custom navbar navbar-default">
-                                <div className="container ">
-                                    <div className="navbar-header">
-                                        <button onClick={this.gotoNote.bind(this)} className="btn btn-link navbar-brand">{this.props.RFQItem.title}</button>
-                                    </div>
-                                </div>
-                            </nav>
-                            <div className="container">
-                                <div className="media">
-                                    <div className="media-left">
-                                        <a href="#"> <img id="rfqimg" src="/RFQ.png" alt="User Image"/> </a>
-                                    </div>
 
-                                    <div className="media-body">
-                                        <p id="dashrfq">RFQ TITLE</p>
-                                        <div>
-                                            <h4>Smoking Kills donot smoke please</h4>
-                                        </div>
-                                        <p id="dashrfq">Date</p>
-                                        <div>
-                                            <h4>7 March, 2017</h4>
-                                        </div>
-                                    </div>
-                                    <div className="media-right media-middle">
-                                        <button type="button" className="btn btn-danger btn-xs">More Info</button>
-                                    </div>
-                                </div>
+            <div id="flag" className="">
+                <div className="container">
+                    <div className="media">
+                        <div className="media-left">
+                            <a href="#"> <img id="rfqimg" src="/RFQ.png" alt="User Image"/> </a>
+                        </div>
+
+                        <div className="media-body">
+                            <p id="dashrfq">RFQ TITLE</p>
+                            <div>
+                                <h4>Smoking Kills donot smoke please</h4>
+                            </div>
+                            <p id="dashrfq">Date</p>
+                            <div>
+                                <h4>7 March, 2017</h4>
+                            </div>
+                        </div>
+                        <div className="media-right">
+                            <div id="btndash">
+                                <button type="button" className="btn btn-danger btn-xs">More Info</button>
+                            </div>
+                            <div id="btndash">
+                                <p>Days Left</p>
+                            </div>
+                            <div id="btndash">
+                                <button type="button" className="btn btn-danger btn-xs">Apply for RFQ</button>
                             </div>
                         </div>
 
-                        <div id="sidejumbotron" className="col-md-6 jumbotron">
-                            <nav className=" navbar-custom navbar navbar-default">
-                                <div className="container">
-                                    <div className="navbar-header">
-                                        <button className="btn btn-link navbar-brand "> RFQ</button>
-                                    </div>
-                                </div>
-                            </nav>
-                            <div className="container">
-                                <div className="media">
-                                    <div className="media-left">
-                                        <a href="#"> <img id="rfqimg" src="/RFQ.png" alt="User Image"/> </a>
-                                    </div>
-
-                                    <div className="media-body">
-                                        <p id="dashrfq">RFQ TITLE</p>
-                                        <div>
-                                            <h4>Smoking Kills donot smoke please</h4>
-                                        </div>
-                                        <p id="dashrfq">Date</p>
-                                        <div>
-                                            <h4>7 March, 2017</h4>
-                                        </div>
-                                    </div>
-                                    <div className=" media-right media-middle">
-                                        <button type="button" className="btn btn-danger btn-xs">More Info</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
+
+
         );
     }
 }
