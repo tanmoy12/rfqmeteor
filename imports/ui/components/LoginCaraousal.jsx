58c20b033b585c5e86d71929d2f40cff2037ed13
tanmoy12 <tanmoy.buet12@gmail.com>
2017-04-26 10:41:20 +0600

    carousel added

--- imports/ui/components/LoginCaraousal.jsx
+++ imports/ui/components/LoginCaraousal.jsx
@@ -4,33 +4,54 @@ import ReactDOM from 'react-dom';
 
 export default class LoginCaraousal extends Component {
 
+    autoplayFunc(){
+        $('#myCarousel').carousel({
+            interval: 3000,
+            cycle: true
+        });
+    }
+
     render() {
         return (
-            <div className="container" >
+            <div className="container" style={{padding: "0",width: "100%"}}>
 
-                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                <div id="myCarousel" className="carousel slide" data-ride="carousel" onMouseOver={this.autoplayFunc.bind(this)}>
+
                     <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
                     </ol>
+
                     <div className="carousel-inner" role="listbox">
-                        <div className="carousel-item active">
-                            <img className="d-block img-fluid" src="..." alt="First slide"/>
+                        <div className="item active">
+                            <img className="d-block img-fluid" src="/img1.jpg" alt="First slide" style={{width: "100%"}}/>
+                            <div className=" carousel-caption">
+                                <h3>DRICM</h3>
+                                <p>Designated Reference Institute for Chemical Measurements</p>
+                            </div>
                         </div>
-                        <div className="carousel-item">
-                            <img className="d-block img-fluid" src="..." alt="Second slide"/>
+                        <div className="item">
+                            <img className="d-block img-fluid" src="/img2.jpg" alt="Second slide"  style={{width: "100%"}}/>
+                            <div className=" carousel-caption">
+                                <h3>DRICM</h3>
+                                <p>Designated Reference Institute for Chemical Measurements</p>
+                            </div>
                         </div>
-                        <div className="carousel-item">
-                            <img className="d-block img-fluid" src="..." alt="Third slide"/>
+                        <div className="item">
+                            <img className="d-block img-fluid" src="/img3.jpg" alt="Third slide"  style={{width: "100%"}}/>
+                            <div className=" carousel-caption">
+                                <h3>DRICM</h3>
+                                <p>Designated Reference Institute for Chemical Measurements</p>
+                            </div>
                         </div>
                     </div>
-                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                        <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                         <span className="sr-only">Previous</span>
                     </a>
-                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                        <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                         <span className="sr-only">Next</span>
                     </a>
                 </div>
