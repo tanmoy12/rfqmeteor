db5544e3e22c2edbc353ea965ddd88af7c87d4f7
tanmoy12 <tanmoy.buet12@gmail.com>
2017-06-01 15:40:03 +0600

    dashboard updated

--- imports/ui/components/Dash.jsx
+++ imports/ui/components/Dash.jsx
@@ -27,18 +27,66 @@ class Dash extends Component {
         return (
             <div>
                 <div className="container">
-                    <div id="Dasboardjumbo" className="jumbotron text-center">
-                        <h1 className="page-header"> Designated Reference for Institute of Chemical Measurements</h1>
-                        <button onClick={this.createRFQ.bind(this)} className="btn btn-primary"
+                    <div style={{marginTop : "3px"}} className="pull-right">
+                        <button id="shadowPut" onClick={this.createRFQ.bind(this)} className="btn btn-primary"
                                 data-toggle="modal" data-target="#popupwindow">
-                            <span className="glyphicon glyphicon-edit"></span> Create
-                            RFQ
+                            <span className="glyphicon glyphicon-edit"></span> Create an
+                            RFQ today
                         </button>
-
                     </div>
-                    {this.renderRFQs()}
+                    <ul className="nav nav-tabs">
+                        <li className="active"><a data-toggle="tab" href="#home">Running RFQs</a></li>
+                        <li><a data-toggle="tab" href="#menu1">Complete RFQs</a></li>
+                    </ul>
+
+                    <div className="tab-content">
+                        <div id="home" className="tab-pane fade in active">
+                            <div>
+                                <input style={{width: "50%", marginTop : "10px", fontSize: "15px"}} className='form-control'
+                                       placeholder='Search RFQs' type='text'
+                                    />
+                            </div>
+                            <div id="Dasboardjumbo" className="jumbotron text-center">
+                                <h1 className="page-header"> Designated Reference for Institute of Chemical Measurements</h1>
+                                <button onClick={this.createRFQ.bind(this)} className="btn btn-primary"
+                                        data-toggle="modal" data-target="#popupwindow">
+                                    <span className="glyphicon glyphicon-edit"></span> Create
+                                    RFQ
+                                </button>
+                            </div>
+                            <div id="Dasboardjumbo" className="jumbotron text-center">
+                                <h1 className="page-header"> Designated Reference for Institute of Chemical Measurements</h1>
+                                <button onClick={this.createRFQ.bind(this)} className="btn btn-primary"
+                                        data-toggle="modal" data-target="#popupwindow">
+                                    <span className="glyphicon glyphicon-edit"></span> Create
+                                    RFQ
+                                </button>
+                            </div>
+                            <div id="Dasboardjumbo" className="jumbotron text-center">
+                                <h1 className="page-header"> Designated Reference for Institute of Chemical Measurements</h1>
+                                <button onClick={this.createRFQ.bind(this)} className="btn btn-primary"
+                                        data-toggle="modal" data-target="#popupwindow">
+                                    <span className="glyphicon glyphicon-edit"></span> Create
+                                    RFQ
+                                </button>
+                            </div>
+                            <div id="Dasboardjumbo" className="jumbotron text-center">
+                                <h1 className="page-header"> Designated Reference for Institute of Chemical Measurements</h1>
+                                <button onClick={this.createRFQ.bind(this)} className="btn btn-primary"
+                                        data-toggle="modal" data-target="#popupwindow">
+                                    <span className="glyphicon glyphicon-edit"></span> Create
+                                    RFQ
+                                </button>
+                            </div>
+                        </div>
+                        <div id="menu1" className="tab-pane fade">
+                            {this.renderRFQs()}
+                        </div>
+                    </div>
+
 
                 </div>
+                <Footer/>
             </div>
         );
     }
