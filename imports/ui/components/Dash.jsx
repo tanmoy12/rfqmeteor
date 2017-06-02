3a705675b5fac31e5c07cc2954035f8a7d487973
tanmoy12 <tanmoy.buet12@gmail.com>
2017-06-02 16:22:34 +0600

    new commit

--- imports/ui/components/Dash.jsx
+++ imports/ui/components/Dash.jsx
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import {createContainer} from 'meteor/react-meteor-data';
 import ReactDOM from 'react-dom';
 import Footer from './Footer';
+import Sidebar from './SideBar';
 
 import RFQBox from './RFQBox';
 
@@ -26,65 +27,42 @@ class Dash extends Component {
     render() {
         return (
             <div>
-                <div className="container">
-                    <div style={{marginTop : "3px"}} className="pull-right">
-                        <button id="shadowPut" onClick={this.createRFQ.bind(this)} className="btn btn-primary"
-                                data-toggle="modal" data-target="#popupwindow">
-                            <span className="glyphicon glyphicon-edit"></span> Create an
-                            RFQ today
-                        </button>
+                <div className="container fade in active">
+                    <div className="col-md-3">
+                        <Sidebar/>
                     </div>
-                    <ul className="nav nav-tabs">
-                        <li className="active"><a data-toggle="tab" href="#home">Running RFQs</a></li>
-                        <li><a data-toggle="tab" href="#menu1">Complete RFQs</a></li>
-                    </ul>
+                    <div className="col-md-9">
+                        <div style={{marginTop : "3px"}} className="pull-right">
+                            <button id="shadowPut" onClick={this.createRFQ.bind(this)} className="btn btn-primary"
+                                    data-toggle="modal" data-target="#popupwindow">
+                                <span className="glyphicon glyphicon-edit"></span> Create an
+                                RFQ today
+                            </button>
+                        </div>
+                        <ul className="nav nav-tabs">
+                            <li className="active"><a data-toggle="tab" href="#home">Running RFQs</a></li>
+                            <li><a data-toggle="tab" href="#menu1">Complete RFQs</a></li>
+                        </ul>
 
-                    <div className="tab-content">
-                        <div id="home" className="tab-pane fade in active">
-                            <div>
-                                <input style={{width: "50%", marginTop : "10px", fontSize: "15px"}} className='form-control'
-                                       placeholder='Search RFQs' type='text'
+                        <div className="tab-content">
+                            <div id="home" className="tab-pane fade in active">
+                                <div>
+                                    <input style={{width: "50%", marginTop : "10px", fontSize: "15px"}} className='form-control'
+                                           placeholder='Search RFQs' type='text'
                                     />
+                                </div>
+                                {this.renderRFQs()}
                             </div>
-                            <div id="Dasboardjumbo" className="jumbotron text-center">
-                                <h1 className="page-header"> Designated Reference for Institute of Chemical Measurements</h1>
-                                <button onClick={this.createRFQ.bind(this)} className="btn btn-primary"
-                                        data-toggle="modal" data-target="#popupwindow">
-                                    <span className="glyphicon glyphicon-edit"></span> Create
-                                    RFQ
-                                </button>
+                            <div id="menu1" className="tab-pane fade">
+                                <div>
+                                    <input style={{width: "50%", marginTop : "10px", fontSize: "15px"}} className='form-control'
+                                           placeholder='Search RFQs' type='text'
+                                    />
+                                </div>
+                                {this.renderRFQs()}
                             </div>
-                            <div id="Dasboardjumbo" className="jumbotron text-center">
-                                <h1 className="page-header"> Designated Reference for Institute of Chemical Measurements</h1>
-                                <button onClick={this.createRFQ.bind(this)} className="btn btn-primary"
-                                        data-toggle="modal" data-target="#popupwindow">
-                                    <span className="glyphicon glyphicon-edit"></span> Create
-                                    RFQ
-                                </button>
-                            </div>
-                            <div id="Dasboardjumbo" className="jumbotron text-center">
-                                <h1 className="page-header"> Designated Reference for Institute of Chemical Measurements</h1>
-                                <button onClick={this.createRFQ.bind(this)} className="btn btn-primary"
-                                        data-toggle="modal" data-target="#popupwindow">
-                                    <span className="glyphicon glyphicon-edit"></span> Create
-                                    RFQ
-                                </button>
-                            </div>
-                            <div id="Dasboardjumbo" className="jumbotron text-center">
-                                <h1 className="page-header"> Designated Reference for Institute of Chemical Measurements</h1>
-                                <button onClick={this.createRFQ.bind(this)} className="btn btn-primary"
-                                        data-toggle="modal" data-target="#popupwindow">
-                                    <span className="glyphicon glyphicon-edit"></span> Create
-                                    RFQ
-                                </button>
-                            </div>
-                        </div>
-                        <div id="menu1" className="tab-pane fade">
-                            {this.renderRFQs()}
                         </div>
                     </div>
-
-
                 </div>
                 <Footer/>
             </div>
