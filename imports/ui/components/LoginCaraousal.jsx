d21d1296755d2270b232d70d5a7d2ef79f8c52db
BingoApps <l.hadima23@gmail.com>
2017-06-06 03:49:02 +0600

    login carasol changed

--- imports/ui/components/LoginCaraousal.jsx
+++ imports/ui/components/LoginCaraousal.jsx
@@ -2,70 +2,30 @@ import React, {Component, PropTypes} from 'react';
 import {createContainer} from 'meteor/react-meteor-data';
 import ReactDOM from 'react-dom';
 
-export default class LoginCaraousal extends Component {
 
-    autoplayFunc(){
-        $('#myCarousel').carousel({
-            interval: 6000,
-            cycle: true
-        });
-    }
-
-    render() {
-        return (
-            <div className="container" style={{padding: "0",width: "100%"}}>
-
-                <div id="myCarousel" className="carousel slide" data-ride="carousel" onMouseOver={this.autoplayFunc.bind(this)}>
-
-                    <ol className="carousel-indicators">
-                        <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-                        <li data-target="#myCarousel" data-slide-to="1"></li>
-                        <li data-target="#myCarousel" data-slide-to="2"></li>
-                    </ol>
-
-                    <div className="carousel-inner" role="listbox">
-                        <div className="item">
-                            <img className="d-block img-fluid" src="/img1.jpg" alt="Third slide"  style={{height: "100%", width: "100%"}}/>
-                            <div className=" carousel-caption">
-                                <h3>DRICM</h3>
-                                <p>Designated Reference Institute for Chemical Measurements</p>
-                            </div>
-                        </div>
-                        <div className="item active">
-                            <img className="d-block img-fluid" src="/img2.jpg" alt="First slide" style={{height: "100%", width: "100%"}}/>
-                            <div className=" carousel-caption">
-                                <h3>DRICM</h3>
-                                <p>Designated Reference Institute for Chemical Measurements</p>
-                            </div>
-                        </div>
-                        <div className="item">
-                            <img className="d-block img-fluid" src="/img3.jpg" alt="Second slide"  style={{height: "100%", width: "100%"}}/>
-                            <div className=" carousel-caption">
-                                <h3>DRICM</h3>
-                                <p>Designated Reference Institute for Chemical Measurements</p>
-                            </div>
-                        </div>
-                        <div className="item">
-                            <img className="d-block img-fluid" src="/img4.jpg" alt="Third slide"  style={{height: "100%", width: "100%"}}/>
-                            <div className=" carousel-caption">
-                                <h3>DRICM</h3>
-                                <p>Designated Reference Institute for Chemical Measurements</p>
-                            </div>
-                        </div>
+render()
+{
+    return (
+        <div id="header" className="header">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6 col-md-6">
+                        <h1>RFQ eFiling System</h1>
+                        <p>eRFQ is a fully automated request for quotation system. <br/>
+                            It creates all necessary documents with secured document signing techniques, <br/>
+                            real time task notifications, and online RFQ application by companies. <br/>
+                            The ful system is made in the light of constitution of the government of Bangladesh.
+                        </p>
+                        
                     </div>
-                    <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                        <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span className="sr-only">Previous</span>
-                    </a>
-                    <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                        <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span className="sr-only">Next</span>
-                    </a>
-                </div>
+                    <div className="col-lg-6 col-md-6">
+                        <img src="RFQSSS.png"/>
+                    </div>
 
+                </div>
             </div>
 
-
-        );
-    }
+        </div>
+    );
+}
 }
