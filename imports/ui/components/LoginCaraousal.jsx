2478f210717f253af5d1d0161473a74dd7c64a24
tanmoy12 <tanmoy.buet12@gmail.com>
2017-06-06 03:55:48 +0600

    front page

--- imports/ui/components/LoginCaraousal.jsx
+++ imports/ui/components/LoginCaraousal.jsx
@@ -1,31 +1,30 @@
-import React, {Component, PropTypes} from 'react';
-import {createContainer} from 'meteor/react-meteor-data';
-import ReactDOM from 'react-dom';
+import React, {Component, PropTypes} from "react";
+import {createContainer} from "meteor/react-meteor-data";
 
+export default class LoginCaraousal extends Component {
 
-render()
-{
-    return (
-        <div id="header" className="header">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-6 col-md-6">
-                        <h1>RFQ eFiling System</h1>
-                        <p>eRFQ is a fully automated request for quotation system. <br/>
-                            It creates all necessary documents with secured document signing techniques, <br/>
-                            real time task notifications, and online RFQ application by companies. <br/>
-                            The ful system is made in the light of constitution of the government of Bangladesh.
-                        </p>
-                        
+    render() {
+        return (
+            <div id="header" className="header">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6">
+                            <h1>RFQ eFiling System</h1>
+                            <p>eRFQ is a fully automated request for quotation system. <br/>
+                                It creates all necessary documents with secured document signing techniques, <br/>
+                                real time task notifications, and online RFQ application by companies. <br/>
+                                The ful system is made in the light of constitution of the government of Bangladesh.
+                            </p>
+
+                        </div>
+                        <div className="col-lg-6 col-md-6">
+                            <img src="RFQSS.png"/>
+                        </div>
+
                     </div>
-                    <div className="col-lg-6 col-md-6">
-                        <img src="RFQSSS.png"/>
-                    </div>
-
                 </div>
-            </div>
 
-        </div>
-    );
-}
+            </div>
+        );
+    }
 }
